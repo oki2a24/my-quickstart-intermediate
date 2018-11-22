9bc4509afd63b22bb2d9bd3c49f9561f6cfe4af8
oki2a24 <oki2a24@gmail.com>
2018-11-23 08:03:51 +0900

    feat: destroy メソッドをポリシーへ追加する

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -18,4 +18,16 @@ class TaskPolicy
     {
         //
     }
+
+    /**
+     * 指定されたユーザーが指定されたタスクを削除できるか決定
+     *
+     * @param  User  $user
+     * @param  Task  $task
+     * @return bool
+     */
+    public function destroy(User $user, Task $task)
+    {
+        return $user->id === $task->user_id;
+    }
 }
